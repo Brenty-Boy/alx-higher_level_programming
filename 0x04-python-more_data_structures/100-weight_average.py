d478093249a4463933eae0633020a835e9df26fc
Brent Hamsley <brenthamsley@gmail.com>
2024-01-15 14:59:55 -0800

    Main

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,8 +4,8 @@ def weight_average(my_list=[]):
         return 0
     score = 0
     weight = 0
-    for i in my_list:
-        weight += i[0] * i[1]
-        score += i[1]
+    for d in my_list:
+        weight += d[0] * d[1]
+        score += d[1]
         weighted_average = weight / score
         return weighted_average
