9f095da610d673bb6e0cbc085ffdea083e0a3551
Brent Hamsley <brenthamsley@gmail.com>
2024-01-15 15:13:17 -0800

    Main

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,11 +1,7 @@
-#!/usr/bin/python3
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    score = 0
-    weight = 0
-    for d in my_list:
-        weight += d[0] * d[1]
-        score += d[1]
-        weighted_average = weight / score
-        return weighted_average
+    else:
+        numerator = sum([score * weight for score, weight in my_list])
+        denominator = sum([weight for score, weight in my_list])
+        return numerator / denominator
