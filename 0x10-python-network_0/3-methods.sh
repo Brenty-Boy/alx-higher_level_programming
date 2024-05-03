5013e8bf6bd89fb3c5b276e277a1af6d695a4a10
Brenty-Boy <brenthamsley@gmail.com>
2024-05-02 22:35:36 -0700

    Main

---
+++ 0x10-python-network_0/3-methods.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Write a Bash script that takes in a URL and displays
+# all HTTP methods the server will accept.
+
+# You have to use curl.
+curl - sI "$1" | grep "Allow" | cut - d " " - f 2-
